59396375326fa31dafc52f1aeaaa1972e9399883
Tóth Károly <toth.karolycar84@gmail.com>
2024-04-18 13:58:22 +0200

    material_bootstrap_basic information included

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './basic/nav/nav.component';
@@ -8,6 +12,7 @@ import { HomeComponent } from './basic/home/home.component';
 import { FooterComponent } from './basic/footer/footer.component';
 import { WorkersComponent } from './basic/workers/workers.component';
 import { IntroductionComponent } from './basic/introduction/introduction.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -20,10 +25,14 @@ import { IntroductionComponent } from './basic/introduction/introduction.compone
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
--- src/app/basic/workers/workers.component.ts
+++ src/app/basic/workers/workers.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-workers',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class WorkersComponent {
 
+  workerName!: string;
+  post!: string;
+  description!: string;
+  image!: string;
+  phone!: number
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.workerName = params['name'];
+      this.displayWorker(this.workerName);
+    });
+  }
+  displayWorker(workerName: string) {
+    const workers: { [key: string]: { name: string, image: string, description: string, post: string, phone: number } } = {
+      'Farkas Gábor': { name: 'Farkas Gábor', image: '/assets/images/Gábor_Közösképviselő.jpg', description: 'Leírás Farkas Gáborról', post: 'Ügyvezető', phone: +36709057322 },
+      'Prepok Tibor': { name: 'Prepok Tibor', image: '/assets/images/Tibi_1_társasházkezelő.jpg', description: 'Leírás Prepok Tiborról', post: 'Társasházkezelő', phone: +36709057322 },
+      'Schlosser János': { name: 'Schlosser János', image: '/assets/images/János_társasházkezelő.jpg', description: 'Leírás Schlosser Jánosról', post: 'Társasházkezelő', phone: +36709057322 },
+      'Farkasné Vancza Ágnes': { name: 'Farkasné Vancza Ágnes', image: '/assets/images/Farkas Ági_könyvelő.jpg', description: 'Leírás Farkasné Vancza Ágnes', post: 'Könyvelő', phone: +36709057322 },
+      'Borsányiné Szabó Ágnes': { name: 'Borsányiné Szabó Ágnes', image: '/assets/images/Ági_könyvelő.jpg', description: 'Borsányiné Szabó Ágnes', post: 'Könyvelő', phone: +36709057322 },
+      'Varga Rezső': { name: 'Varga Rezső', image: '/assets/images/Rezső_Szoftverfejlesztő.jpg', description: 'Leírás Varga Rezsőről', post: 'Szoftverfejlesztő', phone: +36709057322 },
+  };
+
+    const selectedWorker = workers[workerName];
+    if (selectedWorker) {
+      this.post = selectedWorker.post;
+      this.description = selectedWorker.description;
+      this.image = selectedWorker.image;
+      this.phone = selectedWorker.phone
+    }
+  }
 }
