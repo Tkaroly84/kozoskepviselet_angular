df5ef9c5e3e9642d12d1a4fdd0edfae7a95ea4bc
Tóth Károly <toth.karolycar84@gmail.com>
2024-04-18 13:39:22 +0200

    basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './basic/nav/nav.component';
+import { HomeComponent } from './basic/home/home.component';
+import { FooterComponent } from './basic/footer/footer.component';
+import { WorkersComponent } from './basic/workers/workers.component';
+import { IntroductionComponent } from './basic/introduction/introduction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    FooterComponent,
+    WorkersComponent,
+    IntroductionComponent
   ],
   imports: [
     BrowserModule,
